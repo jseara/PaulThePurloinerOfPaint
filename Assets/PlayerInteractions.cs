090e4c958dea1ab787b3b03fcb8c8835b742a3ec
Justin <jseara2011@gmail.com>
2018-01-21 14:26:50 -0500

    Pseudocode
    psuedocode for interactions and movement

--- Assets/PlayerInteractions.cs
+++ Assets/PlayerInteractions.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class PlayerPickup : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	void onClick()
+	{
+		if(Input.GetKeyDown(key: "G"))
+		{
+			//Pick-up items
+
+			//Send out raycast, nearest item to the player is the one interacted with
+
+			//if(item == Key)
+				//pickup and add to inventory
+			//if(item == bucket)
+				//pickup and add to visible inventory
+			//if(item == well)
+				//remove all current coats of paint
+		}
+		if(Input.GetKeyDown(key: "E"))
+		{
+			//Player applies coat of paint
+			//Display progress bar that updates
+			//Upon update completion, do the following
+			//Check if previous coat of paint was present
+				//If yes, apply mix between two colors
+				//If no, apply color
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
