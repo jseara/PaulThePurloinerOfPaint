fad8c01cdaaeb5b07abe22c25d40bdd0d5208963
Justin <jseara2011@gmail.com>
2018-01-22 14:59:12 -0500

    Updating Game Files
    Player imported, blank background, boundaries implemented.
    
    TO-DO:
    
    Interactibles
    Win/Loss State

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPickup : MonoBehaviour {
-
+public class PlayerInteractions : MonoBehaviour 
+{
 	// Use this for initialization
 	void onClick()
 	{
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float speed;
+	Animator anim;
+	public Bounds boundary;
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	
+	// Update is called once per frame
+	void Update() 
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		//TODO: Animation
+		if(boundary.Contains(transform.position))
+		{
+			var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+			transform.position += move * speed * Time.deltaTime;
+		}
+
+    
+		/*
+				if (Input.GetKeyDown(KeyCode.W))
+		{
+			
+		}
+		else if (Input.GetKeyDown(KeyCode.S))
+		{
+			
+		}
+		else if (Input.GetKeyDown(KeyCode.A))
+		{
+			
+		}
+		else if (Input.GetKeyDown(KeyCode.D))
+		{
+			
+		}
+		 */
+
+	}
+}
